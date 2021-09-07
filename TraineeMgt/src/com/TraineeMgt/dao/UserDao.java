d7997b8e131d00d262980893ea590ab7e0d57ee4
ravikant122 <ravikanttatiwal1999@gmail.com>
2021-09-07 07:20:27 +0530

    Remove duplicated directory

--- TraineeMgt/src/com/TraineeMgt/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.TraineeMgt.dao;
-
-import java.util.List;
-
-import com.TraineeMgt.model.User;
-
-public interface UserDao {
-	public List<User> getAllUsers();
-}
--- TraineeMgt/src/com/TraineeMgt/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.TraineeMgt.service;
-
-import java.util.List;
-
-import com.TraineeMgt.model.User;
-
-public interface UserService {
-	public List<User> getAllUsers();
-}
